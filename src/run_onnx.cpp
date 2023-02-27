46d8a4936ddfb842036c58eb53d1faf9405e22b8
git_unspecified <git@unspecified.com>
2023-02-26 23:38:49 -0600

    Move inference into node

--- src/run_onnx.cpp
+++ src/run_onnx.cpp
@@ -12,6 +12,15 @@
 void dummy(void* _, size_t __) {
   void* Section = __hetero_section_begin();
   void* Wrapper = __hetero_task_begin(1, _, __, 1, _, __);
+
+  // Data
+  std::array<float, width * height> input_image{};
+  // Run
+  std::array<float, 10> results = do_inference(input_image);
+  int64_t result = std::distance(
+      results.begin(), std::max_element(results.begin(), results.end()));
+  std::cout << "Result: " << result << "\n";
+
   __hetero_task_end(Wrapper);
   __hetero_section_end(Section);
 }
@@ -24,15 +33,6 @@ int main(int argc, char** argv) {
   }
   char* model_file_name = argv[1];
   init_model(model_file_name);
-
-  // Data
-  std::array<float, width * height> input_image{};
-
-  // Run
-  std::array<float, 10> results = do_inference(input_image);
-  int64_t result = std::distance(
-      results.begin(), std::max_element(results.begin(), results.end()));
-  std::cout << "Result: " << result << "\n";
 
   // make hpvm compiler happy
   void* DFG = __hetero_launch((void*)dummy, 1, nullptr, 0, 1, nullptr, 0);
