e7822e5bfc9cb3a1a3e02d7e7080724bed67984c
git_unspecified <git@unspecified.com>
2023-03-24 16:17:34 -0500

    Add clean pipeline

--- src/run_onnx.cpp
+++ src/run_onnx.cpp
@@ -14,31 +14,42 @@ using std::cout;
 using std::distance;
 using std::max_element;
 
-void dummy(int* result, size_t __, array<float, width * height>* input_image,
-           size_t ___) {
-  // Run
-  void* Section = __hetero_section_begin();
-
-  // -----------------------------------------
-  void* T1 = __hetero_task_begin(1, input_image, ___, 1, result, __);
-  {
-    array<float, 10> local_res;
-    do_inference_at_stage(0, data_pair(*input_image), data_pair(local_res));
-    *result = distance(local_res.begin(),
-                       max_element(local_res.begin(), local_res.end()));
-    cout << "Result T1: " << *result << "\n";
-  }
-  __hetero_task_end(T1);
-
-  __hetero_section_end(Section);
-}
-
 void debug_mnist(int s, float* out) {
   array<float, 10> local_res;
   for (int i = 0; i < local_res.size(); i++) local_res[i] = out[i];
   int result = distance(local_res.begin(),
                         max_element(local_res.begin(), local_res.end()));
   cout << "Result at stage " << s << ": " << result << "\n";
+}
+
+// clang-format off
+void pipeline_kernel(array<float, 10> *result, size_t result_size
+        array<float, width * height>* input_image_1,size_t input_image_size_1,
+        array<float, width * height>* input_image_2,size_t input_image_size_2,
+        array<float, width * height>* input_image_3,size_t input_image_size_3) {  // clang-format on
+  // Run
+  void* Section = __hetero_section_begin();
+
+  {  // -----------------------------------------
+    void* T = __hetero_task_begin(1, input_image_1, input_image_size_1, 1,
+                                  result, result_size);
+    do_inference_at_stage(0, data_pair(*input_image_1), data_pair(*result));
+    __hetero_task_end(T);
+  }
+  {  // -----------------------------------------
+    void* T = __hetero_task_begin(2, result, result_size, input_image_2,
+                                  input_image_size_2, 1, result, result_size);
+    do_inference_at_stage(1, data_pair(*input_image_2), data_pair(*result));
+    __hetero_task_end(T);
+  }
+  {  // -----------------------------------------
+    void* T = __hetero_task_begin(2, result, result_size, input_image_3,
+                                  input_image_size_3, 1, result, result_size);
+    do_inference_at_stage(2, data_pair(*input_image_3), data_pair(*result));
+    __hetero_task_end(T);
+  }
+
+  __hetero_section_end(Section);
 }
 
 int main(int argc, char** argv) {
@@ -49,23 +60,18 @@ int main(int argc, char** argv) {
   }
 
   init_all_stages();
-  auto dbg = [&](int s, float* out) {
-    array<float, 10> local_res;
-    for (int i = 0; i < local_res.size(); i++) local_res[i] = out[i];
-    int result = distance(local_res.begin(),
-                          max_element(local_res.begin(), local_res.end()));
-    cout << "Result at stage " << s << ": " << result << "\n";
-  };
-  for (int i = 0; i < kNumStage; i++) stages[i].dbg_callback = dbg;
+  for (int i = 0; i < kNumStage; i++) stages[i].dbg_callback = debug_mnist;
 
   // Global data & buffers
-  int result = 0;  // shared output used to specify dependency
-  array<float, width * height> input_image{};
-  input_image.fill(0);
+  array<float, 10> result;  // shared output used to specify dependency
+  array<float, width * height> input_image[kNumStage];
+  for (int i = 0; i < kNumStage; i++) input_image[i].fill(0);
 
   // Launch kernel
-  void* DFG = __hetero_launch((void*)dummy, 2, edge_pair(result),
-                              edge_pair(input_image), 1, edge_pair(result));
+  void* DFG =
+      __hetero_launch((void*)pipeline_kernel, 4, edge_pair(result),
+                      edge_pair(input_image[0]), edge_pair(input_image[1]),
+                      edge_pair(input_image[2]), 1, edge_pair(result));
   __hetero_wait(DFG);
   std::cout << "Done! "
             << "\n";
